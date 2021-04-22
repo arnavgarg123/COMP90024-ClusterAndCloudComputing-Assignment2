b396b3d704f12fa7d685e3099840dae3f3eb482c
arnavgarg123 <arnav.garg9@gmail.com>
2021-04-23 00:57:31 +0530

    Querry based on sport. Try

--- TwitterAndAurinDataHarvestor/app.py
+++ TwitterAndAurinDataHarvestor/app.py
@@ -18,14 +18,16 @@ authorizer.set_access_token(access_token, access_token_secret)
 api = tweepy.API(authorizer, timeout=15, wait_on_rate_limit=True)
 
 all_tweets = []
-search_query = 'place:Melbourne'
+search_query = 'sport OR sports OR football OR soccer OR basketball OR rugby OR netball OR nrl OR cricket OR tennis OR afl OR dancing OR cycling OR cycle OR swim OR swimming'
 geo = api.geo_search(query="Melbourne", granularity="city")[0].id
 print(geo)
 start_time = time.time()
-for tweet in tweepy.Cursor(api.search, q="place:%s" % geo, lang='en').items(2000):
-    all_tweets.append([tweet.text, tweet.created_at, tweet.place])
+for tweet in tweepy.Cursor(api.search, q=search_query + " place:%s" % geo, lang='en').items(2000):
+    all_tweets.append([tweet.user.id, tweet.text, tweet.created_at, tweet.place])
 j = 0
+b=open('sport.json', "a")
 for i in all_tweets:
+    b.write(str(i)+"\n")
     print(j, i)
     j = j + 1
 
