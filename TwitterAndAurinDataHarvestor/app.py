a3a84efb8a9a552d29fe3e4eb2c1d026f6b1e6fd
arnavgarg123 <arnav.garg9@gmail.com>
2021-04-23 16:40:54 +0530

    Added header to each file

--- TwitterAndAurinDataHarvestor/app.py
+++ TwitterAndAurinDataHarvestor/app.py
@@ -1,3 +1,17 @@
+# Assignment 2 - COMP90024 Course at The University of Melbourne
+#
+# Cluster and Cloud Computing - Team 48
+#
+# Authors:
+#
+#  * Arnav Garg (Student ID: 1248298)
+#  * Piyush Bhandula (Student ID: 1163716)
+#  * Jay Dave (Student ID: 1175625)
+#  * Vishnu Priya G (Student ID: 1230719)
+#  * Gurkirat Singh Chohan 1226595
+#
+# Location: India, Melbourne, Singapore
+#
 import re
 import sys
 import time
@@ -18,11 +32,11 @@ authorizer.set_access_token(access_token, access_token_secret)
 api = tweepy.API(authorizer, timeout=15, wait_on_rate_limit=True)
 
 all_tweets = []
-search_query = 'sport OR sports OR football OR soccer OR basketball OR rugby OR netball OR nrl OR cricket OR tennis OR afl OR dancing OR cycling OR cycle OR swim OR swimming'
+search_query = ''
 geo = api.geo_search(query="Melbourne", granularity="city")[0].id
 print(geo)
 start_time = time.time()
-for tweet in tweepy.Cursor(api.search, q=search_query + " place:%s" % geo, lang='en').items(2000):
+for tweet in tweepy.Cursor(api.search, q="place:%s" % geo, lang='en').items(2000):
     all_tweets.append([tweet.user.id, tweet.text, tweet.created_at, tweet.place])
 j = 0
 b=open('sport.json', "a")
