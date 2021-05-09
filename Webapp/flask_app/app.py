64a02e5599e0ed7a727fb189c4acbf3664520229
Piyush Bhandula <14964881+piyushbhandula@users.noreply.github.com>
2021-05-09 11:14:33 +0530

    web app additions

--- Webapp/flask_app/app.py
+++ Webapp/flask_app/app.py
@@ -1,11 +1,47 @@
-from flask import Flask
-from endpoints.sentiment import overall_sentiment
-app=Flask(__name__,template_folder='templates')
-app.register_blueprint(overall_sentiment)
-@app.route("/")
-def home():
-    return "hello"
+import flask
+from flask import request, url_for, render_template, redirect
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+from dominate.tags import img
 
+logo = img(src='./static/img/logo.png', height="50", width="50", style="margin-top:-15px")
+#here we define our menu items
+topbar = Navbar(logo,
+                View('Home', 'my_maps'),
+                View('Scenario 1', 'get_sc1'),
+                View('Scenario 2', 'get_sc2'),
+                View('Scenario 3', 'get_sc3')
+                )
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0')
+nav = Nav()
+nav.register_element('left', topbar)
+
+app = flask.Flask(__name__)
+Bootstrap(app)
+
+@app.route('/',methods=['GET','POST'])
+def my_maps():
+
+  mapbox_access_token = 'pk.eyJ1IjoiZ3Vya2kwOSIsImEiOiJja29iejNiZjkxaWg0MndtdTFiZzdkcXVnIn0.hTqur4keYNgXKLjldLaVEw'
+
+  return render_template('index.html',
+        mapbox_access_token=mapbox_access_token)
+
+@app.route('/sc1', methods=['GET'])
+def get_sc1():
+    return(render_template('temp.html'))
+    
+@app.route('/sc2', methods=['GET'])
+def get_sc2():
+    return(render_template('temp.html'))
+    
+@app.route('/sc3', methods=['GET'])
+def get_sc3():
+    return(render_template('temp.html'))
+
+nav.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
