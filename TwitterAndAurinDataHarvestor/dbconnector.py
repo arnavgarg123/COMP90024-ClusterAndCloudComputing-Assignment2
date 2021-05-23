e735cc0010c5c4c340ffd7d9824923df87d37f45
arnavgarg123 <arnav.garg9@gmail.com>
2021-05-23 12:11:22 +0530

    Added comments and cleaned the code

--- TwitterAndAurinDataHarvestor/dbconnector.py
+++ TwitterAndAurinDataHarvestor/dbconnector.py
@@ -18,16 +18,22 @@ import time
 import random
 
 class Couch:
+    # Establishing Connection with db
     def __init__(self,ip,dbnamelist):
         couchserver=couchdb.Server(url=ip)
         couchserver.resource.credentials=('admin','admin')
+        # Variable to store database objects
         self.db=[]
+        # Pre requisite db
         dbsl=['tweet_api','region']
+        # Reading master node ip
         f=open("ip.txt", "r")
         couchdb_master_ip=f.readline().rstrip()
         couchdb_master_login_url='http://admin:admin@'+couchdb_master_ip+':5984/'
+        # Reading other node ip to enable replication
         db_children=f.readlines()
         f.close()
+        # Creating or loading db
         for dbname in dbsl:
             self.db=self.db+[self.createdb(couchserver,dbname)]
             for child in db_children:
@@ -36,14 +42,17 @@ class Couch:
             self.db=self.db+[self.createdb(couchserver,dbname)]
             for child in db_children:
                 couchserver.replicate(couchdb_master_login_url+dbname,'http://admin:admin@'+child.rstrip()+':5984/'+dbname,create_target=True,continuous=True)
+        # Adding static data to db
         self.create_static()
 
+    # Creating db if it does not exist, else loading it
     def createdb(self,couchserver,dbname):
         if dbname in couchserver:
             return couchserver[dbname]
         else:
             return couchserver.create(dbname)
 
+    # Adding static data to db needed by harvestor
     def create_static(self):
         a=open('tweetapi.json')
         for i in a.readlines():
@@ -60,6 +69,7 @@ class Couch:
             except:
                 pass
 
+    # Pushing mined and cleaned tweets along with their sentiment to db
     def pushdata(self,data,dbname):
         flag=0
         for i in self.db:
@@ -72,10 +82,12 @@ class Couch:
         if flag==1:
             print(dbname+" does not exist")
 
+    # Fetching data and setting lock or flag on item
     def getdata(self,dbname):
         while True:
             for i in self.db:
                 if dbname==i._name:
+                        # Randomly reading data item so that each item is read aprox equal number of times
                         r=str(random.randint(1,19))
                         harvest_obj=i.get(r)
                         if harvest_obj['flag']==0:
@@ -92,6 +104,7 @@ class Couch:
                                 time.sleep(15)
                                 pass
 
+    # Releasing lock or flag after operation on that item is over
     def resetflag(self,column,value,dbname):
         for i in self.db:
             if dbname==i._name:
@@ -101,6 +114,7 @@ class Couch:
                         target['flag']=0
                         i.save(target)
 
+    # Pushing new sinceid to avoid getting same tweets from twitter
     def updatesinceid(self,column,value,since_id):
         for i in self.db:
             if 'region'==i._name:
--- TwitterAndAurinDataHarvestor/sentiment_analyse.py
+++ TwitterAndAurinDataHarvestor/sentiment_analyse.py
@@ -24,26 +24,39 @@ import logging
 tf.get_logger().setLevel(logging.ERROR)
 class sentiment:
     def __init__(self):
+        # Downloading stop words
         nltk.download('stopwords')
+        # Loading tockenizer
         handle=open('tokenizer.pickle', 'rb')
         self.tokenizer1 = pickle.load(handle)
         self.stop_words_test = stopwords.words('english')
+
+    # Cleaning text for analysis model
     def preprocess_test(self,text):
+        # Removing symbols from text
         text = re.sub("@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+", ' ', str(text).lower()).strip()
         cleaned_text = []
         tockens=np.zeros([2,2])
+        # Removing stop words
         for token in text.split():
             if token not in self.stop_words_test:
                 cleaned_text.append(token)
+        # Padding and tockenizing
         tokens=tf.keras.preprocessing.sequence.pad_sequences(self.tokenizer1.texts_to_sequences([" ".join(cleaned_text)],),maxlen = 30)
         return tokens
+
+    # Load created model
     def load_model(self):
         with tf.device('/cpu:0'):
             return tf.keras.models.load_model("model.h5")
+
+    # Sentiment Analysis and putting everything together
     def sentiment_analysis(self,text):
         text=self.preprocess_test(text)
         model=self.load_model()
+        # Using cpu for processing
         with tf.device('/cpu:0'):
             output=model.predict(text)
+        # Clearing memory
         tf.keras.backend.clear_session()
         return output
